a09f7bdb6cf5e54564554b5572115089c43f6505
Daniel Urbina Mac <danriursa@hotmail.com>
2023-03-13 16:03:00 -0500

    Vistas

--- applications/Historial/views.py
+++ applications/Historial/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import  ListView
 from .models import Producto
+from .models import Precio
 # Create your views here.
 
 
@@ -9,3 +10,9 @@ class ProductoListView(ListView):
     model = Producto
     template_name = "Historial/Historial.html"
     context_object_name='historial'
+
+
+class PrecioListView(ListView):
+    model = Precio
+    template_name = "Historial/Precio.html"
+    context_object_name='precio'
